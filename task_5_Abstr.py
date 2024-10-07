47adc01cba955135c97ba59072f5fcbbb1f8dd78
Vlad <vlad07407@gmail.com>
2024-10-07 23:02:41 +0500

    All completed

--- task_5_Abstr.py
+++ task_5_Abstr.py
@@ -12,7 +12,7 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return (3.14) * self.radius ** 2
+        return 3.14 * self.radius ** 2
 
 
 class Rectangle(Shape):
@@ -34,21 +34,19 @@ class Triangle(Shape):
 
 
 if __name__ == '__main__':
-    # Создание экземпляров классов
     circle = Circle(5)
     rectangle = Rectangle(4, 6)
     triangle = Triangle(3, 8)
-    # Вычисление площади фигур
+
     circle_area = circle.area()
     rectangle_area = rectangle.area()
     triangle_area = triangle.area()
-    # Вывод результатов
+
     print("Площадь круга:", circle_area)
     print("Площадь прямоугольника:", rectangle_area)
     print("Площадь треугольника:", triangle_area)
-    # Попытка создания экземпляра абстрактного класса Shape (должно вызвать ошибку)
     try:
-        shape = Shape()  # Ожидается ошибка
+        shape = Shape()
     except TypeError as e:
         print(f"Ошибка: {e}")
 
